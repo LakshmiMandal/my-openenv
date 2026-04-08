a328ff859cf3c7ab2bcfcee8f374cf1a93034e2f
LakshmiMandal <45812178+LakshmiMandal@users.noreply.github.com>
2026-04-08 20:45:54 +0530

    Add files via upload

--- rl_env_demo/server/app.py
+++ rl_env_demo/server/app.py
@@ -83,12 +83,4 @@ def main(host: str = "0.0.0.0", port: int = 8000):
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--port", type=int, default=8000)
-    args = parser.parse_args()
-    main(host=args.host, port=args.port)
-
-
+    main()
