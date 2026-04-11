2ae8024ac098e36bfa136b643cae24565d1bc891
LakshmiMandal <45812178+LakshmiMandal@users.noreply.github.com>
2026-04-11 15:30:32 +0530

    Add files via upload

--- rl_env_demo/__init__.py
+++ rl_env_demo/__init__.py
@@ -8,7 +8,30 @@
 
 from .client import SmartHomeEnv
 from .models import SmartHomeAction, SmartHomeObservation
+from .tasks import (
+    get_grader,
+    get_task_config,
+    ALL_TASKS,
+    grade_cost_minimization,
+    grade_comfort_optimization,
+    grade_sustainability,
+)
 
-__all__ = ["SmartHomeEnv", "SmartHomeAction", "SmartHomeObservation"]
+# Also import graders from graders.py to ensure they're accessible
+# when referenced as "graders:grade_cost_minimization" in openenv.yaml
+from . import graders
+
+__all__ = [
+    "SmartHomeEnv",
+    "SmartHomeAction",
+    "SmartHomeObservation",
+    "get_grader",
+    "get_task_config",
+    "ALL_TASKS",
+    "grade_cost_minimization",
+    "grade_comfort_optimization",
+    "grade_sustainability",
+    "graders",
+]
 
 
